c26f014d9bf715282356adef11359fa714eab2a9
Yashwanth Thota <thota5366@gmail.com>
2026-06-04 19:59:00 +0530

    Create MovieDetails.jsx

---
+++ src/MovieDetails.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const MovieDetails = () => {
+  const { id } = useParams();
+
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=77df4299&i=${id}`
+      );
+
+      const data = await res.json();
+      setMovie(data);
+    };
+
+    getMovie();
+  }, [id]);
+
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className="details-page">
+
+  <img
+    src={movie.Poster}
+    alt={movie.Title}
+  />
+
+  <div>
+    <h1>{movie.Title}</h1>
+
+    <p><strong>Year:</strong> {movie.Year}</p>
+
+    <p><strong>Genre:</strong> {movie.Genre}</p>
+
+    <p><strong>Actors:</strong> {movie.Actors}</p>
+
+    <p><strong>Director:</strong> {movie.Director}</p>
+
+    <p><strong>IMDb Rating:</strong> {movie.imdbRating}</p>
+
+    <p><strong>Plot:</strong> {movie.Plot}</p>
+  </div>
+
+</div>
+  );
+};
+
+export default MovieDetails;
